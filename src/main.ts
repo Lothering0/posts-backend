55bf343d5cec49987db4315fba2eed15e750428d
Danila <danil2005232@gmail.com>
2022-12-31 23:02:24 +0300

    Defining port and message about successfully started app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function main() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    console.info(`Server started on port ${PORT}`);
+  });
 }
 
 main();
