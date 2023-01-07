0cd8cbd5d34538d3b21b5398f93022dfcdfe9401
Danila <danil2005232@gmail.com>
2023-01-07 14:36:54 +0300

    Typization .env variables

---
+++ typings/global.d.ts
@@ -0,0 +1,15 @@
+export declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+
+      POSTGRES_HOST: string;
+      POSTGRES_USER: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_DB: string;
+      POSTGRES_PORT: string;
+
+      PRIVATE_KEY: string;
+    }
+  }
+}
